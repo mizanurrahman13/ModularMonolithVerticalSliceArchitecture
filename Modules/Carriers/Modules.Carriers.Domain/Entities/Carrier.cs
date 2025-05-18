e0a04c06ac7a4f1ce6786467731241aa15b6c287
mizanurrahman13 <mizan.441980@gmail.com>
2025-05-18 15:59:34 +0600

    Added Carriers Modules Domain layer project

---
+++ Modules/Carriers/Modules.Carriers.Domain/Entities/Carrier.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Modules.Carriers.Domain.Entities;
+
+public class Carrier
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public List<CarrierShipment> Shipments { get; set; } = [];
+}
---
+++ Modules/Carriers/Modules.Carriers.Domain/Entities/CarrierShipment.cs
@@ -0,0 +1,11 @@
+﻿namespace Modules.Carriers.Domain.Entities;
+
+public class CarrierShipment
+{
+    public Guid Id { get; set; }
+    public Guid CarrierId { get; set; }
+    public string OrderId { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public Carrier Carrier { get; set; } = null!;
+    public Address ShippingAddress { get; set; } = null!;
+}
